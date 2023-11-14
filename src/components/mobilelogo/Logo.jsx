ed6be89c734c7f43da5108fdaaf1ef2a724e7ab5
Martin <papapopal522@gmail.com>
2023-11-14 16:47:43 +0200

    mob2

---
+++ src/components/mobilelogo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MobileLogo = () => {
+    return (
+        <img src={require('../../assets/mtrading2.png')} width={28} height={22} draggable={false}/>
+    );
+};
+
+export default MobileLogo;
